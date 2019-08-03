ad289db3d1b1de0a9bbd1ce674ac4306906ff1a3
yawarwar <yawarwar@gmail.com>
2019-08-04 03:03:30 +0500

    Remove .idea directories

--- java2.3.Booleans/src/Loader.java
+++ java2.3.Booleans/src/Loader.java
@@ -1,8 +1,6 @@
 
-public class Loader
-{
-    public static void main(String[] args)
-    {
+public class Loader {
+    public static void main(String[] args) {
         Integer milkAmount = 300; // ml
         Integer powderAmount = 500; // g
         Integer eggsCount = 30; // items
@@ -10,18 +8,15 @@ public class Loader
         Integer oilAmount = 30; // ml
         Integer appleCount = 8;
 
-        if (powderAmount >= 400 && sugarAmount >= 10 && milkAmount >=1 && oilAmount >= 30)
-        {
+        if (powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1 && oilAmount >= 30) {
             //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
             System.out.println("Pancakes");
-        } else if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5)
-        {
+        } else if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5) {
             //milk - 300 ml, powder - 5 g, eggs - 5
             System.out.println("Omelette");
-        } else if (appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4)
-        {
+        } else if (appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4) {
             //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-        System.out.println("Apple pie");
+            System.out.println("Apple pie");
         }
     }
 }
